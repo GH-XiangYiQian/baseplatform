bfa4347d4a5263c4e76a9f19744c86ba5449d461
Administrator <Administrator@USER-20181107VY>
2019-06-19 15:47:58 +0800

    多余代码删除

--- src/main/java/com/platform/basics/controller/UserLoginController.java
+++ src/main/java/com/platform/basics/controller/UserLoginController.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
